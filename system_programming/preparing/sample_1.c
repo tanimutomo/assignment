9139560b99816c0852a375e4d5576f49a3ad3f53
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2019-04-10 18:44:02 +0900

    install gcc compiler and test done

---
+++ system_programming/preparing/sample_1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void) {
+    // String
+    char var;
+    var = 'A';
+    printf("var is %c\n", var);
+
+    // Integer
+    int a, b, c;
+    a = 22;
+    printf("a is %d\n", a);
+
+    // float
+    float x;
+    float y;
+    float z;
+    x = 33.33;
+    printf("x is %f\n", x);
+
+    // double
+    double sum;
+    sum = 44.444444444;
+    printf("sum is %f\n", sum);
+
+    return 0;
+}
+
