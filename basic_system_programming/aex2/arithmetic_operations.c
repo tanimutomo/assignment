140e4980e215b1e981bd272589c65c3e6be43835
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2019-05-13 18:29:40 +0900

    add the codes of 4th class

---
+++ basic_system_programming/aex2/arithmetic_operations.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void sort(int *a, int *b, int *c) {
+    int tmp;
+
+    return;
+}
+
+int main() {
+    int p, q, r;
+    p = 1; q = 2, r = 3;
+    sort(&p, &q, &r);
+    printf("In main: p=%d, q=%d, r=%d\n", p, q, r);
+    exit(0);
+}
