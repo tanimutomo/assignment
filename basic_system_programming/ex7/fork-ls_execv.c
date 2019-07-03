57195cc3764dc866421b26c7ba6f085d3c28ce50
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2019-07-04 01:20:47 +0900

    Add the 11th assignment script

---
+++ basic_system_programming/ex7/fork-ls_execv.c
@@ -0,0 +1,25 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  pid_t pid;
+
+  printf("Now, create new process \n");
+  pid = fork();
+  if (pid < 0) {
+    exit(1);
+  }
+  if (pid == 0) {
+    sleep(1);
+    char *const args[] = {"/bin/ls", ".", NULL};
+    printf("child's process id = %d\n", (int)getpid());
+    execv(args[0], args);
+  } else {
+    printf("parent's process id = %d\n", (int)getpid());
+    printf("parent process terminated.\n");
+    exit(0);
+  }
+  exit(0);
+}
